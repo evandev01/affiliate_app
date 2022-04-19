d060c481ae6781b4dde12cdf2825be9301fa10cc
Evan Bero <63209633+evandev01@users.noreply.github.com>
2022-04-19 14:24:29 -0500

    added redux, frontend authentication

--- server/server.js
+++ server/server.js
@@ -2,11 +2,14 @@ const express = require('express')
 const connectDB = require('../config/db')
 const routes = require('./routes')
 const colors = require('colors')
+const cors = require('cors')
 require('dotenv').config()
 
 const app = express()
 
 app.use(express.json())
+
+app.use(cors())
 
 connectDB()
 
