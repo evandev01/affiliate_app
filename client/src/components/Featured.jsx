06aec83fc169d263f692dbf25f58800a4002377c
Evan Bero <63209633+evandev01@users.noreply.github.com>
2022-05-25 17:11:47 -0500

    styled home page

--- client/src/components/Featured.jsx
+++ client/src/components/Featured.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { Row, Col, Button } from 'react-bootstrap'
 import Product from './Product'
@@ -6,13 +6,23 @@ import Message from '../components/Message'
 import Loader from '../components/Loader'
 
 const Featured = ({ type, title, products, user, loading, error, message }) => {
+  const [loadingTitle, setLoadingTitle] = useState(false)
+
+  useEffect(() => {
+    if (!title) {
+      setLoadingTitle(true)
+    } else {
+      setLoadingTitle(false)
+    }
+  }, [title])
+
   return (
     <>
       {error && <Message variant='danger'>{message}</Message>}
-      {loading && <Loader />}
+      {loading || (loadingTitle && <Loader />)}
       <Row className='mt-5 mb-2' id={`border`}>
         <Col>
-          <h5 id='featured'>{title}</h5>
+          <h5 id='featured'>{title && title}</h5>
         </Col>
       </Row>
       <Row className='mt-3 mb-2'>
